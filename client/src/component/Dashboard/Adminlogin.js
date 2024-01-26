d9e798af0cd73075da19fca7717078b4c6f33a8c
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-01-27 00:01:57 +0530

    Update Adminlogin.js

--- client/src/component/Dashboard/Adminlogin.js
+++ client/src/component/Dashboard/Adminlogin.js
@@ -10,7 +10,7 @@ const Adminlogin = () => {
     const loginUser = async (e) => {
         e.preventDefault();
 
-        const res = await fetch('http://localhost:5000/login', {
+        const res = await fetch('https://blogv2server.onrender.com/login', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
