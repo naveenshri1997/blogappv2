9d7a4e2a1835fe6728ca90eea5a87cd065a20b8e
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-02-01 21:00:49 +0530

    Delete client/src directory

--- client/src/component/Privateroute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-
-const Privateroute = () => {
-    const auth = localStorage.getItem('user');
-    return auth?<Outlet />:<Navigate to="adminlogin"/>
-}
-
-export default Privateroute
